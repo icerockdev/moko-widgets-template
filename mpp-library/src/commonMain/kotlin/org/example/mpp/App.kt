ba3faf74516a49e494dc8ee69b732c38681f6dec
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2020-01-23 15:03:10 +0700

    update code to new moko-widgets

--- mpp-library/src/commonMain/kotlin/org/example/mpp/App.kt
+++ mpp-library/src/commonMain/kotlin/org/example/mpp/App.kt
@@ -7,17 +7,14 @@ package org.example.mpp
 import dev.icerock.moko.widgets.core.Theme
 import dev.icerock.moko.widgets.screen.Args
 import dev.icerock.moko.widgets.screen.BaseApplication
-import dev.icerock.moko.widgets.screen.Screen
-import kotlin.reflect.KClass
+import dev.icerock.moko.widgets.screen.ScreenDesc
 
 class App : BaseApplication() {
-    override fun setup() {
+    override fun setup(): ScreenDesc<Args.Empty> {
         val theme = Theme()
 
-        registerScreenFactory(MainScreen::class) { MainScreen(theme) }
-    }
-
-    override fun getRootScreen(): KClass<out Screen<Args.Empty>> {
-        return MainScreen::class
+        return registerScreen(MainScreen::class) {
+            MainScreen(theme)
+        }
     }
 }
