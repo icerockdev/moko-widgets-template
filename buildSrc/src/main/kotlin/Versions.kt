a617b9bfc6e42fa4203253825d7e530fc496a4d2
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2020-01-28 15:46:10 +0700

    update versions of widgets and units

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -13,8 +13,8 @@ object Versions {
 
     private const val mokoResources = "0.6.2"
     private const val mokoNetwork = "0.3.0"
-    private const val mokoUnits = "0.2.0-dev-3"
-    private const val mokoWidgets = "0.1.0-dev-6"
+    private const val mokoUnits = "0.2.1"
+    private const val mokoWidgets = "0.1.0-dev-7"
 
     object Plugins {
         const val kotlin = Versions.kotlin
